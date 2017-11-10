7a4947bbea2c02cc2ca2f7385b02ac1592b44519
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-11-10 15:46:35 +0100

    Added console_script to enable direct start of the service

--- setup.py
+++ setup.py
@@ -50,5 +50,8 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
+    entry_points={
+        'console_scripts': ['ringo_auth=ringo_auth.server:run']
+    }
 )
